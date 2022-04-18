042ce983752ee5d951078b333ecc5cc924ae166b
priyan.perera@gmail.com <priyan.perera@gmail.com>
2022-04-19 08:25:47 +1000

    Add signup and login pages.

---
+++ WebAdvert.Web/Controllers/AccountController.cs
@@ -0,0 +1,116 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Account;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<CognitoUser> signInManager;
+        private readonly UserManager<CognitoUser> userManager;
+        private readonly CognitoUserPool pool;
+
+        public AccountController(SignInManager<CognitoUser> signInManager,
+            UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.pool = pool;
+        }
+
+        public async Task<ActionResult> SignUp()
+        {
+            var model = new SignUpViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists.");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Confirm", "Account");
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> Confirm()
+        {
+            var model = new ConfirmViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Confirm(ConfirmViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with the given email address was not found.");
+                    return View(model);
+                }
+
+                var result = await (userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> LogIn()
+        {
+            var model = new LoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("LoginError", "Email and password do not match");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ WebAdvert.Web/Models/Account/SignUpViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Account
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirmation do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
